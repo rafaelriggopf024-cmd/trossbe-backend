01637b9ffac73f7efe3b2c9c41deea907631844e
Rafael Riggo <20221PF.INF_I0024@passofundo.ifsul.local>
2025-12-11 16:23:39 -0300

    Fix CORS for Vercel

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
+
 app.use(express.json());
 
 const SUPABASE_URL = process.env.SUPABASE_URL || "https://amxzaxhdnabtgkitkejq.supabase.co";
